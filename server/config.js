1231406b43f69d6e734a0061deae20d26e8cb7a5
Max Lam <agnusmaximus@Maxs-MacBook-Pro.local>
2013-06-08 11:09:29 -0700

    Added server side details that allow facebook login to run

---
+++ server/config.js
@@ -0,0 +1,9 @@
+Accounts.loginServiceConfiguration.remove({
+  service: "facebook"
+});
+
+Accounts.loginServiceConfiguration.insert({
+	service: "facebook",
+	appId: "387037424734804",
+	secret: "ae4d377e14df43e8a014cefbae86b298"
+});
